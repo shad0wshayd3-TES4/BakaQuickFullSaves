6343479e0345ee9b35a4a26c7e86e687a8f968d0
shad0wshayd3 <2724172+shad0wshayd3@users.noreply.github.com>
2025-06-12 10:05:58 -0600

    feat: Address Library support

--- src/main.cpp
+++ src/main.cpp
@@ -6,21 +6,22 @@ namespace Hooks
 	private:
 		static bool SaveGame(void* a_this, void* a_file, const char* a_fileName, bool a_logStats)
 		{
-			if (a_fileName && !_stricmp(a_fileName, "quicksave")) {
+			if (a_fileName && !_stricmp(a_fileName, "quicksave"))
+			{
 				a_fileName = nullptr;
 			}
 			return _SaveGame00(a_this, a_file, a_fileName, a_logStats);
 		}
 
-		inline static REL::Hook _SaveGame00{ REL::Offset(0x656B8D0), 0x050, SaveGame };
-		inline static REL::Hook _SaveGame01{ REL::Offset(0x656EE20), 0x031, SaveGame };
-		inline static REL::Hook _SaveGame02{ REL::Offset(0x67010D0), 0x056, SaveGame };
-		inline static REL::Hook _SaveGame03{ REL::Offset(0x6713EF0), 0x00C, SaveGame };
-		inline static REL::Hook _SaveGame04{ REL::Offset(0x671A180), 0x1CC, SaveGame };
-		inline static REL::Hook _SaveGame05{ REL::Offset(0x67AC550), 0x096, SaveGame };
-		inline static REL::Hook _SaveGame06{ REL::Offset(0x67AD900), 0x252, SaveGame };
-		inline static REL::Hook _SaveGame07{ REL::Offset(0x67ADC50), 0x023, SaveGame };
-		inline static REL::Hook _SaveGame08{ REL::Offset(0x6A12220), 0x157, SaveGame };
+		inline static REL::Hook _SaveGame00{ REL::ID(405145), 0x050, SaveGame };
+		inline static REL::Hook _SaveGame01{ REL::ID(405249), 0x031, SaveGame };
+		inline static REL::Hook _SaveGame02{ REL::ID(409582), 0x056, SaveGame };
+		inline static REL::Hook _SaveGame03{ REL::ID(409667), 0x00C, SaveGame };
+		inline static REL::Hook _SaveGame04{ REL::ID(409710), 0x1CC, SaveGame };
+		inline static REL::Hook _SaveGame05{ REL::ID(411233), 0x096, SaveGame };
+		inline static REL::Hook _SaveGame06{ REL::ID(411246), 0x252, SaveGame };
+		inline static REL::Hook _SaveGame07{ REL::ID(411247), 0x023, SaveGame };
+		inline static REL::Hook _SaveGame08{ REL::ID(421554), 0x157, SaveGame };
 	};
 
 	class hkQuickLoad :
@@ -30,7 +31,7 @@ namespace Hooks
 		static void RequestLoadLastSaveGame(void* a_this)
 		{
 			using func_t = decltype(&RequestLoadLastSaveGame);
-			static REL::Relocation<func_t> func{ REL::Offset(0x48C9EE0) };
+			static REL::Relocation<func_t> func{ REL::ID(308794) };
 			return func(a_this);
 		}
 
@@ -39,7 +40,7 @@ namespace Hooks
 			return RequestLoadLastSaveGame(a_this);
 		}
 
-		inline static REL::Hook _RequestQuickLoad{ REL::Offset(0x4890A30), 0xC6, RequestQuickLoad };
+		inline static REL::Hook _RequestQuickLoad{ REL::ID(308184), 0xC6, RequestQuickLoad };
 	};
 
 	class hkQuickLoadCheck :
@@ -49,7 +50,7 @@ namespace Hooks
 		static void GetSlotNameFromIndex(void* a_this, void* a_slotName, std::int32_t a_slotIndex)
 		{
 			using func_t = decltype(&GetSlotNameFromIndex);
-			static REL::Relocation<func_t> func{ REL::Offset(0x48BCF30) };
+			static REL::Relocation<func_t> func{ REL::ID(308656) };
 			return func(a_this, a_slotName, a_slotIndex);
 		}
 
@@ -59,7 +60,7 @@ namespace Hooks
 			return _DoesSaveExist(a_this, a_slotName);
 		}
 
-		inline static REL::Hook _DoesSaveExist{ REL::Offset(0x48C9F30), 0x32, DoesSaveExist };
+		inline static REL::Hook _DoesSaveExist{ REL::ID(308795), 0x32, DoesSaveExist };
 	};
 }
 
